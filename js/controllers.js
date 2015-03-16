bcf419c24b2f879daf82d2c02d8325ee40ce3b2b
unknown <tiagomvieira@gmail.com>
2015-03-16 23:00:31 +0000

    Adicionei sons. Corrigi bugs e adicionei umas classes de selecionar e resposta certa.

--- js/controllers.js
+++ js/controllers.js
@@ -2,6 +2,9 @@ angular.module('nome.controllers', [])
     .controller('jogoCtrl', ["$scope", "Rest", "$timeout", "$routeParams", "$location", "$window",
         function($scope, Rest, $timeout, $routeParams, $location, $window) {
 
+            var correct = new Audio('../sounds/correct.mp3');
+            var wrong = new Audio('../sounds/wrong.mp3');
+
             $scope.range = function(n) {
                 return new Array(n);
             };
@@ -23,6 +26,11 @@ angular.module('nome.controllers', [])
             $scope.reload = function() {
                 $window.location.reload();
             };
+
+            // ao fechar o modal temos que re-iniciar o jogo
+            $(document).on('closed.fndtn.reveal', '[data-reveal]', function () {
+                $scope.reload();
+            });
 
             $scope.pontuacao = 0;
             $scope.total = -1;
@@ -52,17 +60,18 @@ angular.module('nome.controllers', [])
                 $scope.sorteado2 = nome2;
             }
 
+            $scope.ultimaRespostaCorrecta = '';
             $scope.clicked = function(number){
                 if($scope.blocked) return
-                else
-                    $scope.blocked = true
+                else $scope.blocked = true
 
-                if(number == 0){selectedWord = $scope.sorteado1}
-                else{selectedWord = $scope.sorteado2}
+                if(number == 0){$scope.selectedWord = $scope.sorteado1}
+                else{$scope.selectedWord = $scope.sorteado2}
 
-                Rest.postAnswer(selectedWord, $scope.sorteado1, $scope.sorteado2).then(
+                Rest.postAnswer($scope.selectedWord, $scope.sorteado1, $scope.sorteado2).then(
                     function(data){
-                        // criar aqui o link de share
+                        // acende a resposta correcta
+
                         $scope.challengeLink = "https://twitter.com/intent/tweet?";
                         $scope.challengeLink += "original_referer=" + encodeURIComponent( $location.absUrl() ) +"&";
                         $scope.challengeLink += "text="+ encodeURIComponent( "Qual é o nome correcto? " + $scope.sorteado1 + " ou " + $scope.sorteado2 ) +"&";
@@ -72,13 +81,22 @@ angular.module('nome.controllers', [])
 
                         if(data.answer){
                             $("body").css("background-color","#91e49e");
-                            $scope.pontuacao += 1;
+                            $scope.pontuacao += 100;
                             $scope.venceuUltimo = true;
+                            $scope.ultimaRespostaCorrecta = $scope.selectedWord;
+                            correct.play();
                         }
                         else{
+                            wrong.play();
                             $("body").css("background-color","#f48a71");
                             $scope.venceuUltimo = false;
                             $scope.vidas -= 1
+
+                            if($scope.selectedWord==$scope.sorteado1){
+                                $scope.ultimaRespostaCorrecta = $scope.sorteado2;
+                            } else {
+                                $scope.ultimaRespostaCorrecta = $scope.sorteado1;
+                            }
 
                             if ($scope.vidas <= 0) {
                                 console.log($location.absUrl());
@@ -89,7 +107,6 @@ angular.module('nome.controllers', [])
                                 $scope.scoreLink += "tw_p=tweetbutton&";
                                 $scope.scoreLink += "url="+ encodeURIComponent( "http://" + $location.host() ) +"&";
                                 $scope.scoreLink += "via=totonome";
-                                console.log($scope.scoreLink);
                                 $('#myModal').foundation('reveal', 'open');
                             }
                         }
@@ -107,13 +124,15 @@ angular.module('nome.controllers', [])
                             $scope.ultimo2 = $scope.sorteado2;
                             $scope.sorteado1 = data.word1;
                             $scope.sorteado2 = data.word2;
-                            $scope.blocked = false
+                            $scope.ultimaRespostaCorrecta = '';
+                            // reset aos botões
+                            if($scope.vidas>0) $scope.blocked = false
                         }, 500);
                     },
                     function(data){
 
                     }
-                    )
+                )
             }
         }
     ]);
